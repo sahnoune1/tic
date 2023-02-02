d600d138dcec879bd70d8b396d1ebdacae6df7be
seif <116313275+sahnoune1@users.noreply.github.com>
2023-02-02 22:20:07 +0100

    index.html

--- java.js
+++ java.js
@@ -13,23 +13,107 @@ function createArray(...elements) {
   };
 }
 
-const array = createArray(1, 2, 3, 4, 5, 6, 7, 8, 9);
-console.log(array.elements);
-console.log(array.elements[5]);
-console.log(array.elements.length);
+const player = createArray("x", "x", "x", "x", "x", "x", "x", "x", "x");
+const enemy = createArray("o", "o", "o", "o", "o", "o", "o", "o", "o");
+console.log(player.elements);
+console.log(player.elements[5]);
+console.log(player.elements.length);
 
-// let j = 0;
-// for (j = 0; j < array.elements.length; j++) {
-//   const fill = document.querySelector(`.item${j}`);
-//   fill.innerHTML = array.elements[j];
-// }
-
-let x = 0;
-for (x = 0; x < 9; x++) {
+const me = document.querySelector(".player");
+const you = document.querySelector(".enemy");
+for (let x = 0; x < 9; x++) {
   const fil = document.querySelector(`.item${x}`);
 
   fil.addEventListener("click", () => {
-    const y = x;
-    fil.innerHTML = array.elements[y];
+    if (!fil.classList.contains("filled")) {
+      if (me.classList.contains("selected")) {
+        fil.innerHTML = player.elements[x];
+
+        fil.classList.add("filled");
+      } else if (you.classList.contains("selected")) {
+        fil.innerHTML = enemy.elements[x];
+
+        fil.classList.add("filled");
+      }
+      const test = [];
+      for (let y = 0; y < 9; y++) {
+        test[y] = document.querySelector(`.item${y}`);
+      }
+
+      if (
+        test[0].innerHTML === "x" &&
+        test[1].innerHTML === "x" &&
+        test[2].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+      if (
+        test[3].innerHTML === "x" &&
+        test[4].innerHTML === "x" &&
+        test[5].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+      if (
+        test[6].innerHTML === "x" &&
+        test[7].innerHTML === "x" &&
+        test[8].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+      if (
+        test[0].innerHTML === "x" &&
+        test[3].innerHTML === "x" &&
+        test[6].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+      if (
+        test[1].innerHTML === "x" &&
+        test[4].innerHTML === "x" &&
+        test[7].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+
+      if (
+        test[2].innerHTML === "x" &&
+        test[5].innerHTML === "x" &&
+        test[8].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+      if (
+        test[0].innerHTML === "x" &&
+        test[4].innerHTML === "x" &&
+        test[8].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+      if (
+        test[2].innerHTML === "x" &&
+        test[4].innerHTML === "x" &&
+        test[6].innerHTML === "x"
+      ) {
+        alert("we have a winner");
+        location.reload();
+      }
+    }
   });
 }
+
+you.addEventListener("click", () => {
+  you.classList.add("selected");
+  me.classList.remove("selected");
+});
+me.addEventListener("click", () => {
+  me.classList.add("selected");
+  you.classList.remove("selected");
+});
